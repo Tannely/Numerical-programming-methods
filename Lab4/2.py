cbd0fbf7abd93a6fa912d1f5fc58aba025cdd7f8
Tannely <taniakorchaga@gmail.com>
2021-09-29 23:15:41 +0300

    Matrix task 2

---
+++ Lab4/2.py
@@ -0,0 +1,42 @@
+from random import randint
+import numpy as np
+N = 4
+M = 5
+A = [[0] * M for i in range (N)]
+print('Матриця заповнена випадковими числами\n') 
+for i in range (N):
+    for j in range (M):
+        A[i][j] = randint(3, 23)
+        print(A[i][j], end = ' ')   
+    print()
+sum = 0
+for i in range (N):
+    for j in range (M):
+        sum += A[i][j]
+print('\nСума елементів матриці =',sum)
+print('\nCереднє значення стовпців')
+c = np.mean(A, axis = 0)
+print(c)
+print('\nCереднє значення рядків')
+c1 = np.mean(A, axis = 1)
+print(c1)
+min = A[0][0]
+min_i = 0
+min_j = 0
+max = A[0][0]
+max_i = 0
+max_j = 0
+for i in range (N):
+    for j in range (M):
+        if A[i][j] < min:
+            min = A[i][j]
+            min_i = i
+            min_j = j
+        if A[i][j] > max:
+            max = A[i][j]
+            max_i = i
+            max_j = j     
+print('\nМінімальний елемент матриці = ', min)
+print('Індекс мінімального значення = [{}, {}]'.format(min_i, min_j))
+print('\nМаксимальний елемент матриці = ', max)
+print('Індекс максимального значення значення = [{}, {}]'.format(max_i, max_j))
